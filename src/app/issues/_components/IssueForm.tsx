d3be1c7d9a683be1a471514239b751bbaf4aa7c5
aayush <hiaayush30@gmail.com>
2025-06-23 16:57:29 +0530

    updating issue

--- src/app/issues/_components/IssueForm.tsx
+++ src/app/issues/_components/IssueForm.tsx
@@ -1,6 +1,6 @@
 "use client"
-import React, { useState } from "react"
-import { Button, Callout, TextField } from "@radix-ui/themes"
+import React, { useEffect, useState } from "react"
+import { Box, Button, Callout, TextField } from "@radix-ui/themes"
 import dynamic from "next/dynamic";
 // all components are rendered on server once (even client side ones)
 // so components like SimpleMDE which use browser apis like navigator gives issues
@@ -16,13 +16,19 @@ import { createIssueSchema } from "@/lib/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/components/ErrorMessage";
 import Spinner from "@/components/Spinner";
-import { Issue } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import { DropdownMenu } from "@radix-ui/themes"
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
     ssr: false
 })
 
 function IssueForm({ issue }: { issue?: Issue }) {
+    const [status, setStatus] = useState<null | Status>(null);
+    useEffect(() => {
+        if (issue) setStatus(issue.status);
+    }, [issue])
+
     const router = useRouter();
     const [error, setError] = useState('');
     const [isSubmitting, setIsSubmitting] = useState(false);
@@ -34,6 +40,12 @@ function IssueForm({ issue }: { issue?: Issue }) {
             setIsSubmitting(true);
             // createIssue(data) not needed here but good for complex apps where you use same call
             // via multiple components specially sending certain http headers
+            if (issue) {
+                await axios.patch("/api/issues/" + issue.id, { ...data, status });
+                router.push("/issues");
+                toast.success("issue updated successfully");
+                return;
+            }
             await axios.post("/api/issues", data);
             router.push("/issues");
             toast.success("issue created successfully");
@@ -77,9 +89,41 @@ function IssueForm({ issue }: { issue?: Issue }) {
                 />
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-                <Button disabled={isSubmitting}>
-                    {isSubmitting ? <>Submitting <Spinner /></> : "Submit New Issue"}
-                </Button>
+                <div className="flex items-center gap-4">
+                    {issue && (
+                        <Box>
+                            <DropdownMenu.Root>
+                                <DropdownMenu.Trigger>
+                                    <Button variant="soft">
+                                        {status == "IN_PROGRESS" ? "IN PROGRESS" : status}
+                                        <DropdownMenu.TriggerIcon />
+                                    </Button>
+                                </DropdownMenu.Trigger>
+                                <DropdownMenu.Content>
+                                    <DropdownMenu.Item
+                                        color="red"
+                                        onClick={() => setStatus("OPEN")}>
+                                        OPEN
+                                    </DropdownMenu.Item>
+                                    <DropdownMenu.Item
+                                        color="violet"
+                                        onClick={() => setStatus("IN_PROGRESS")}>
+                                        IN PROGRESS
+                                    </DropdownMenu.Item>
+                                    <DropdownMenu.Item
+                                        color="green"
+                                        onClick={() => setStatus("CLOSED")}>
+                                        CLOSED
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Content>
+                            </DropdownMenu.Root>
+                        </Box>
+                    )}
+
+                    <Button disabled={isSubmitting}>
+                        {isSubmitting ? <> Submitting <Spinner /></> : issue ? "Edit Issue" : "Submit New Issue"}
+                    </Button>
+                </div>
             </form>
         </div>
     )
