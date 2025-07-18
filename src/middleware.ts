5dc4e119e6ef80523cebf7662df5b60bcfc497bc
aayush <hiaayush30@gmail.com>
2025-07-18 14:00:49 +0530

    commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,6 @@ export { default } from 'next-auth/middleware';
 
 export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request, secret: process.env.NEXT_AUTH_SECRET });
-    const url = request.nextUrl;
 
     if (!token) {
         return NextResponse.redirect(new URL('/signin', request.url));
